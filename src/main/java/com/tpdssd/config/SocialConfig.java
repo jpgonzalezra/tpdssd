a8fa196b2e664bab542c6dfc79dcdef9e4f93c0e
Joaquin Gonzalez <joaquingonzalez@despegar.com>
2016-10-19 10:45:41 -0300

    config

--- src/main/java/com/tpdssd/config/SocialConfig.java
+++ src/main/java/com/tpdssd/config/SocialConfig.java
@@ -64,7 +64,7 @@ public class SocialConfig {
 		// openshift
 		// registry.addConnectionFactory(new GoogleConnectionFactory("532257042908-905s9h7gvmtu0mep5m0e5ums8l7f9tt2.apps.googleusercontent.com", "qgxnkfDpro-Me7ii2UzcNwkA"));
 		// local
-		registry.addConnectionFactory(new GoogleConnectionFactory("532257042908-1tr5pbgqgfeq4239do2n5m7a88jss8kr.apps.googleusercontent.com", "d8Vhe9sJKxhh4AkS0ukaVg_1"));
+		registry.addConnectionFactory(new GoogleConnectionFactory("532257042908-905s9h7gvmtu0mep5m0e5ums8l7f9tt2.apps.googleusercontent.com", "qgxnkfDpro-Me7ii2UzcNwkA"));
 		return registry;
 	}
 	
